f4136bd563e5c2f14ea629e7a7d07f241eb01524
Casper Huang <casper.w.huang@qq.com>
2025-06-14 20:10:25 +0800

    feat: support URL parameter for temp tex

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 const perc = ref(72)
 useAppLocalStorage('perc', perc)
+
+const params = useUrlSearchParams<URLParams>('history')
+onMounted(async () => {
+  if (!params.tex)
+    return
+  appState.active = ''
+  await nextTick()
+  appState.update(params.tex!)
+})
 </script>
 
 <template>
